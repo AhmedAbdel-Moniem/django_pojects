fb521932e8bb63e126a01421b25b990b059b032a
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2020-12-22 13:14:37 +0200

    OOP

--- coding_files/main.py
+++ coding_files/main.py
@@ -1,2 +1,39 @@
-def f():
-	print('hi')
+from __future__ import print_function
+from employees import PizzaRobot, Server
+
+
+class Customer:
+    def __init__(self, name):
+        self.name = name
+
+    def order(self, server):
+        print(self.name, "orders from ", server)
+
+    def pay(self, server):
+        print(self.name, 'pays for item to ', server)
+
+
+class Oven:
+    def bake(self):
+        print('oven bakes')
+
+
+class PizzaShop:
+    def __init__(self):
+        self.server = Server('pat')  # Embed other objects
+        self.chef   = PizzaRobot('bob')
+        self.oven   = Oven()
+
+    def order(self, name):
+        customer = Customer(name)  # Activate other objects
+        customer.order(self.server)
+        self.chef.work()
+        self.oven.bake()
+        customer.pay(self.server)
+
+
+if __name__ == "__main__":
+    scene = PizzaShop()
+    scene.order('Homer')
+    print('...')
+    scene.order('Shaggy')
