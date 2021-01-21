bca66096969f598a69b611cb6387e665fb7b1d6e
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-01-22 01:17:47 +0200

    blog

--- blog/mysite/blog/models.py
+++ blog/mysite/blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
+
+
+# Model Manager
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset() \
+                                            .filter(status='published')
 
 
 class Post(models.Model):
@@ -8,18 +16,30 @@ class Post(models.Model):
         ('draft', 'draft'),
         ('published', 'published'),
     )
-    title  = models.CharField(max_length=250)
-    slug   = models.SlugField(max_length=250, unique_for_date='publish')
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
-                               on_delete    = models.CASCADE,
-                               related_name = 'blog_posts')
-    body    = models.TextField()
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
+    body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    status  = models.CharField(max_length=10,
-                              choices = STATUS_CHOICES,
-                              default = 'draft')
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    objects = models.Manager()  # default manager
+    published = PublishedManager()  # custom mngr
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[
+                           self.publish.year,
+                           self.publish.month,
+                           self.publish.day,
+                           self.slug
+                       ]
+                       )
 
     class Meta:
         ordering = ('-publish',)
