5c88a09fa2e56b3ee8d64d6a316e6222054b0844
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-01-24 00:32:22 +0200

    sitemap

--- blog/mysite/mysite/settings.py
+++ blog/mysite/mysite/settings.py
@@ -15,7 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-
+SITE_ID = 1
 # Application definition
 
 INSTALLED_APPS = [
@@ -27,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
