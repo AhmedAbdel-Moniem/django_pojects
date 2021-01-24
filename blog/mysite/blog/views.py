f0355598f073ad0c4a290ce15f34c4f17de5bc7f
AhmedAbdel-Moniem <ahmed.moname@gmail.com>
2021-01-24 23:25:17 +0200

    trigram_search

--- blog/mysite/blog/views.py
+++ blog/mysite/blog/views.py
@@ -12,7 +12,7 @@ from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm, SearchForm
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import TrigramSimilarity, SearchVector, SearchQuery, SearchRank
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -88,12 +88,16 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
-            search_vector = SearchVector('title', 'body')
+            search_vector = SearchVector('title', weight='A') + \
+                            SearchVector('body', weight='B')
             search_query = SearchQuery(query)
+            # results = Post.published.annotate(
+            #     search=search_vector,
+            #     rank=SearchRank(search_vector, search_query)
+            # ).filter(rank__gte=.3).order_by('-rank')
             results = Post.published.annotate(
-                search=search_vector,
-                rank=SearchRank(search_vector, search_query)
-            ).filter(search=search_query).order_by('-rank')
+                similarity=TrigramSimilarity('title', query),
+            ).filter(similarity__gt=0.1).order_by('-similarity')
     return render(request, 'blog/post/search.html', {
         'form': form,
         'query': query,
